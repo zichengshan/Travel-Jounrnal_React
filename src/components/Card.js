97cd54643513857610af877e9d3706c71a307cb9
Zicheng.S <zichengshan@gmail.com>
2022-04-13 15:29:44 -0700

    css for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,8 +10,8 @@ export default function Card() {
                     <a className="card--location--map" href="https://goo.gl/maps/1DGM5WrWnATgkSNB8">View on Google Map </a>
                 </div>
                 <h2 className="card--title">Mount</h2>
-                <p className="card--time">date</p>
-                <p className="card--body">Mount</p>
+                <p className="card--time">12 Jan, 2021 - 24 Jan, 2021</p>
+                <p className="card--body">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
             </div>
         </div>
 
